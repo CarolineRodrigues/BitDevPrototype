2050a8ffa20e1112a3cca3d4cc31eac1ffa00366
Caroline Rodrigues <c_costa_rodrigues@hotmail.com>
2022-03-30 11:52:08 +0100

    bit dev

---
+++ ticapp-ds/node_modules/@ticapp-scope/ds.footer/_src/footer.composition.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer } from './footer';
+
+export const BasicFooter = () => {
+  return (
+    <Footer>hello world!</Footer>
+  );
+}
+
+export const BasicFooter2 = () => {
+  return (
+    <Footer classCSS = 'primary'>hello world!2</Footer>
+  );
+}
